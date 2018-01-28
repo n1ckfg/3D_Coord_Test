20252db852eb1c752b05f46d5cce150cd44c657c
Nick Fox-Gieg <nick@fox-gieg.com>
2018-01-27 23:21:28 -0500

    first

---
+++ Assets/Scripts/MatchView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchView : MonoBehaviour {
+
+    public Transform target;
+    public Camera cam;
+
+    private void Update() {
+        Ray ray = cam.ScreenPointToRay(target.position);
+        transform.position = ray.origin;
+    }
+
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public float range = 1f;
+    public float speed = 0.1f;
+
+    private Vector3 target = Vector3.zero;
+
+    private void Start() {
+        randomize();
+    }
+	
+	void Update() {
+        transform.position = Vector3.Lerp(transform.position, target, speed);
+        if ((int) (transform.position.x * 10) == (int) (target.x * 10)) randomize();
+	}
+
+    void randomize() {
+        target = new Vector3(Random.Range(-range, range), Random.Range(-range, range), Random.Range(-range, range));
+    }
+
+}
